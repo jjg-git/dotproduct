755072381db6448b6aebc0e8636d7e43f8f6fd70
ramosronn <ronn_ramos@dlsu.edu.ph>
2024-03-26 13:02:17 +0800

    Added visual studio folder

---
+++ dotProduct/main.c
@@ -0,0 +1,13 @@
+// Template C to x86 call
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+extern void asmhello();
+
+int main(int argc, char* argv[]) {
+
+	asmhello();
+	
+	return 0;
+}
